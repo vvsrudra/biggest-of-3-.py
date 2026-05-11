868ac046aa45a9f46b93c19eaa5e78bbf552bf05
VVS RUDRA <vvsrudra@gmail.com>
2026-05-11 21:28:38 +0530

    Add files via upload

---
+++ biggest of 3 .py
@@ -0,0 +1,9 @@
+a = int(input("enter the number "))
+b = int(input("enter the number "))
+c = int(input("enter the number "))
+if ((a>b and a>c)):
+     print("a is latgest ") 
+elif ((b>a and b>c )):
+    print("b is largest")
+else :
+    print ("c is largest ")
